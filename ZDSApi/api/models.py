f3297abf6b5782838146e550aad915aab33dab0d
zhou <16300720005@fudan.edu.cn>
2019-05-15 22:29:23 +0800

    add models.py

--- ZDSApi/api/models.py
+++ ZDSApi/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Users(models.Model):
+    user_name = models.CharField(max_length=50)
+    bitmap = models.CharField(max_length=1005, default=1000*'0')
+    word_collected = models.TextField()
+    dictionary_collected = models.TextField()
+    yiju_collected = models.TextField()
+    setting_new_word = models.IntegerField(default=10)
+    setting_review_word = models.IntegerField(default=10)
+    study_history = models.TextField()
+    mode = models.IntegerField(default=0) #0表示积累模式
+    def __str__(self):
+        return self.user_name
+
+class Word(models.Model):
+    word = models.CharField(max_length=20)
+    meaning = models.TextField()
+    sentence = models.TextField()
+    article = models.TextField()
+    book_id = models.IntegerField()
+    part_of_speech = models.TextField() #词性
+    def __str__(self):
+        return self.word
+
+class Dictionary(models.Model):
+    word = models.CharField(max_length=20)
+    meaning = models.TextField()
+    sentence = models.TextField()
+    source = models.TextField()
+    part_of_speech = models.TextField() #词性
+    def __str__(self):
+        return self.word
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Yiju(models.Model):
+    date = models.DateTimeField()
+    title = models.TextField()
+    dynasty = models.TextField()
+    author = models.TextField()
+    article = models.TextField()
+    content = models.TextField()
+    like = models.IntegerField(default=0)
+    def __self__(self):
+        return self.title
+
+
+
