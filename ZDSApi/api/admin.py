1f4fda120ba6ac6f89bb02d2a3fa4acd8cc98a08
Bernard Shen <jcshen1230@126.com>
2019-05-15 22:52:05 +0800

    update admin.py

--- ZDSApi/api/admin.py
+++ ZDSApi/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Users, Word, Dictionary, Book, Yiju
+
+admin.site.register(Users)
+admin.site.register(Word)
+admin.site.register(Dictionary)
+admin.site.register(Book)
+admin.site.register(Yiju)
+
 
 # Register your models here.
