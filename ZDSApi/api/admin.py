61017e2025aa56f2f0521c0c3aa5559de60becf2
zhou <16300720005@fudan.edu.cn>
2019-05-26 23:37:11 +0800

    alter admin.py

--- ZDSApi/api/admin.py
+++ ZDSApi/api/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Users, Word, Dictionary, Book, Yiju
+from .models import Users, Word, Dictionary, Book, Yiju, TempSave
 
 admin.site.register(Users)
 admin.site.register(Word)
 admin.site.register(Dictionary)
 admin.site.register(Book)
 admin.site.register(Yiju)
-
+admin.site.register(TempSave)
 
 # Register your models here.
