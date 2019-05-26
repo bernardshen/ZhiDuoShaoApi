f7f82800cf555ab662b8fb7b97f43d18b4d26d4e
gzc <1040843613@qq.com>
2019-05-26 22:54:24 +0800

    date

--- ZDSApi/api/views.py
+++ ZDSApi/api/views.py
@@ -23,6 +23,56 @@ from datetime import datetime
 import json
 from django.db.models import Sum, Count
 
+date_map = {
+    0: '〇',
+    1: '一',
+    2: '二',
+    3: '三',
+    4: '四',
+    5: '五',
+    6: '六',
+    7: '七',
+    8: '八',
+    9: '九'
+}
+  
+def chinese2digits(num, type):
+    str_num = str(num)
+    result = ''
+    if type == 0:
+        for i in str_num:
+            result = '{}{}'.format(result, date_map.get(int(i)))
+    if type == 1:
+        result = '{}十{}'.format(date_map.get(int(str_num[0])), date_map.get(int(str_num[1])))
+    if type == 2:
+        result = '十{}'.format(date_map.get(int(str_num[1])))
+    if type == 3:
+        result = '十'
+    if type == 4:
+        result = '二十'
+    return result
+
+def ch_date(date_year,date_month,date_day):
+    year=chinese2digits(date_year,0)
+    if date_month == 10:
+        month = chinese2digits(date_month, 3)
+    if date_month > 10:
+        month = chinese2digits(date_month, 2)
+    if date_month < 10:
+        month = chinese2digits(date_month, 0)
+    date_day = datetime.datetime.now().day
+    if date_day < 10:
+        day = chinese2digits(date_day, 0)
+    if 10 < date_day < 20:
+        day = chinese2digits(date_day, 2)
+    if date_day > 20:
+        day = chinese2digits(date_day, 1)
+    if date_day == 10:
+        day = chinese2digits(date_day, 3)
+    if date_day == 20:
+        day = chinese2digits(date_day, 4)
+    return year,month,day
+
 
 ERROR_CODE = {
     'success':           0, 
@@ -316,14 +366,14 @@ class YijuEveryday(APIView):
         for yiju in yijus:
             data={
                 "push_id":      yiju.id,
-                "date":         yiju.date,
+                "date":         ch_date(date_year=yiju.date.year,date_month=yiju.date.month,date_day=yiju.date.day),
                 "dynasty":      yiju.dynasty,
                 "author":       yiju.author,
                 "title":        yiju.title,
                 "article":      yiju.article,
                 "content":      yiju.content,
                 "like_count":   yiju.like,
-                "like":         yiju.id in collect
+                "like":         int(yiju.id in collect)
             }
             yiju_list.append(data)
         
